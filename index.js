41ce116c6598f1a62b5ee81e0ee5068f0d8a982f
1763manm-beep <1763manm@gmail.com>
2025-09-27 09:54:02 +0000

    Add specific routes for HTML pages to handle clean URLs and file name matching

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Index.html'));
 });
 
+app.get('/courses', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Courses.html'));
+});
+
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Search.html'));
+});
+
+app.get('/testseries', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Test Series.html'));
+});
+
+app.get('/careers', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Careers.html'));
+});
+
+app.get('/research', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Research.HTML'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'About.html'));
+});
+
+app.get('/course', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Course.html'));
+});
+
 // Listen
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
